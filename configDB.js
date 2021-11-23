bdff4cb22987b114880ea55491e0f3679a6433ae
AdminHudsys <94926895+AdminHudsys@users.noreply.github.com>
2021-11-23 15:16:47 -0300

    Update configDB.js

--- configDB.js
+++ configDB.js
@@ -2,7 +2,7 @@ var config = {
     host: 'db-mysql-nyc1-87788-do-user-8243629-0.b.db....',
     database: 'DbPacom',
     user: 'doadmin',
-    password: 'mg7xlo3jy3rnmoay',
+    password: 'mg7xlo3jy3rnmoay234',
     multipleStatements:true,
     port: '25060',
     queryTimeout: 3000,
